4eca1634874f4931a52d0ef534e20fd1062a94ea
ASALEEMKHAN <asaleemkhan386@gmail.com>
2025-01-27 20:27:43 +0530

    Day-5

--- JS/03_basics/04_iife.js
+++ JS/03_basics/04_iife.js
@@ -8,3 +8,18 @@
 ((name) => {
   console.log(`DB CONNECTED TWO ${name}`);
 })("HITESH");
+
+let val1 = 10
+let val2 = 5
+
+function addNum(num1, num2) {
+  let total = num1 + num2
+  return total
+}
+
+let result1 = addNum(val1, val2)
+let result2 = addNum(10, 2)
+
+console.log("Ans1:",result1);
+console.log("Ans2:",result2);
+
---
+++ JS/04_control_flow/switch.js
@@ -0,0 +1,52 @@
+// Switch Case
+
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3; // Number type
+
+// switch (month) {
+//   case 1:
+//     console.log("January");
+//     break;
+//   case 2:
+//     console.log("February");
+//     break;
+//   case 3:
+//     console.log("March");
+//     // break;
+//   case 4:
+//     console.log("April");
+//     break;
+
+//   default:
+//     console.log("default case match");
+//     break;
+// }
+
+const month = "April"; // String type
+
+switch (month) {
+  case "January":
+    console.log("January");
+    break;
+  case "February":
+    console.log("February");
+    break;
+  case "March":
+    console.log("March");
+    break;
+  case "April":
+    console.log("April");
+    break;
+
+  default:
+    console.log("default case match");
+    break;
+}
