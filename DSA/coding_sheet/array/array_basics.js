1c1edb2c59d8303ad07e1af679f9230e777f8b8c
ASALEEMKHAN <asaleemkhan386@gmail.com>
2025-02-08 20:44:29 +0530

    (Day-14, DSA-7)

---
+++ DSA/coding_sheet/array/array_basics.js
@@ -0,0 +1,115 @@
+/*
+1.Given an array arr. Your task is to find the minimum and maximum elements in the array.
+
+Note: Return a Pair that contains two elements the first one will be a minimum element and the second will be a maximum.
+
+Examples:
+
+Input: arr[] = [3, 2, 1, 56, 10000, 167]
+Output: 1 10000
+Explanation: minimum and maximum elements of array are 1 and 10000.
+
+Input: arr[] = [1, 345, 234, 21, 56789]
+Output: 1 56789
+Explanation: minimum and maximum element of array are 1 and 56789.
+
+Input: arr[] = [56789]
+Output: 56789 56789
+Explanation: Since the array contains only one element so both min & max are same.
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(1)
+*/
+
+function minimumNumber(arr, n) {
+  let smallestNum = arr[0];
+
+  for (let i = 1; i < n; i++) {
+    if (arr[i] < smallestNum) {
+      smallestNum = arr[i];
+    }
+  }
+  return smallestNum;
+}
+
+function maximumNumber(arr, n) {
+  let largestNum = arr[0];
+
+  for (let i = 1; i < n; i++) {
+    if (arr[i] > largestNum) {
+      largestNum = arr[i];
+    }
+  }
+  return largestNum;
+}
+
+function getMinimumAndMaximum(n, arr) {
+  let smallestNum = minimumNumber(arr, n);
+  let largestNum = maximumNumber(arr, n);
+  return [smallestNum, largestNum];
+}
+
+let inp_1 = [3, 2, 1, 56, 10000, 167];
+
+let result_1 = getMinimumAndMaximum(inp_1.length, inp_1);
+
+console.log("Ans: 1");
+
+console.log(result_1);
+
+/*
+2.Given an array, arr of positive integers. Find the third largest element in it. Return -1 if the third largest element is not found.
+
+Examples:
+
+Input: arr[] = [2, 4, 1, 3, 5]
+Output: 3
+Explanation: The third largest element in the array [2, 4, 1, 3, 5] is 3.
+
+Input: arr[] = [10, 2]
+Output: -1
+Explanation: There are less than three elements in the array, so the third largest element cannot be determined.
+
+Input: arr[] = [5, 5, 5]
+Output: 5
+Explanation: In the array [5, 5, 5], the third largest element can be considered 5, as there are no other distinct elements.
+
+Expected Time Complexity: O(n)
+Expected Space Complexity: O(1)
+*/
+
+function thirdLargest(arr) {
+  let n = arr.length;
+  if (n <= 2) {
+    return -1;
+  }
+
+  let fLargest = Math.max(arr[0], arr[1]);
+  let sLargest = Math.min(arr[0], arr[1]);
+  let tLargest = Number.MIN_SAFE_INTEGER;
+
+  for (let i = 2; i < n; i++) {
+    if (arr[i] > tLargest) {
+      tLargest = arr[i];
+    }
+
+    if (arr[i] > sLargest) {
+      tLargest = sLargest;
+      sLargest = arr[i];
+    }
+
+    if (arr[i] > fLargest) {
+      sLargest = fLargest;
+      fLargest = arr[i];
+    }
+  }
+  return tLargest;
+}
+
+let inp_2 = [2, 4, 1, 3, 5];
+
+console.log("Ans: 2");
+
+console.log(thirdLargest(inp_2));
+console.log(thirdLargest([10, 2]));
+console.log(thirdLargest([5, 5, 5]));
